dbb016c23f618cd7907b1dbc5f65f73c3771573b
HeeGon Chae <chaeheegon5858@gmail.com>
2024-09-05 13:51:21 +0900

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,14 @@
+import os
+
+from pydantic.v1 import BaseSettings
+
+
+class Settings(BaseSettings):
+    userid: str = ''
+    passwd: str = ''
+    dbname: str = 'clouds2024'
+    dburl: str = ''
+    dbconn: str = f'sqlite:///app/{dbname}.db'
+
+config = Settings()
+
